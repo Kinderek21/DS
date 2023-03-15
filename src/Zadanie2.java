c266d072a51390f22ef8a4175422b26c2afcb9ae
NORBERT GIERCZAK <norbertgierczak@997.com>
2023-03-15 14:12:52 +0100

    Initial commit

---
+++ src/Zadanie2.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+
+        //15.03.2023
+        System.out.println( "Damian ,Sejud");
+        System.out.println("JakasUlica, 500"  );
+        System.out.println("NowySacz, 33-300");
+        System.out.println("123456789");
+    }
+    }
